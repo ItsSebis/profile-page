a8fdeee0f7a046872a538a3ffaa5e6f13d40d384
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-01-31 13:02:29 +0100

    1.0.2 first paragraph

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 include_once "header.html";
 ?>
 
-<title>ItsSebis ∘ Home</title>
+<head><title>ItsSebis ∘ Home</title></head>
 
-<div class="main" style="margin-top: 60px;">
-    <h1></h1>
+<div class="main" style="margin-top: 60px; text-align: center">
+    <h1>ItsSebis</h1>
+    <h2>Ich kann:</h2>
+    <ul style="width: 45%; margin: 0 auto; text-align: left">
+        <li>Websites erstellen (PHP, HTML, CSS)</li>
+        <li>Discord Server verwalten (User/Role management)</li>
+        <li>Discord Bots programmieren (JDA)</li>
+        <li>Minecraft plugins programmieren (Spigot/Paper)</li>
+    </ul>
 </div>
