43327372871003882aafa3d3396aaaa9d46e46b6
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-02-06 20:33:18 +0100

    Statistics #6

--- sharky/stats.php
+++ sharky/stats.php
@@ -6,7 +6,7 @@ require_once "extensions/functions.php";
 <div class="stats">
     <h2>Statistics</h2>
     <p>Servers: <?php echo(getGuildCount()); ?></p>
-    <p>Saved Members: <?php echo(getMemberCount()); ?></p>
+    <p>Saved Members: <?php echo(allMemberCount()); ?></p>
     <p>Read Messages: <?php echo(readMessageCount()); ?></p>
     <p>Read Words: <?php echo(readWordCount()); ?></p>
     <p>Numbers counted: <?php echo(countedNumbers()); ?></p>
