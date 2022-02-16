98c3a004bbe2133e8a7b535de680f71fb355e3b3
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-02-16 20:02:35 +0100

    Better now

--- projects/index.php
+++ projects/index.php
@@ -6,66 +6,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&family=Roboto+Mono:wght@300&display=swap" rel="stylesheet">
     <link rel="icon" href="../img/title-bar.png">
+    <link rel="stylesheet" href="style.css">
 </head>
-<style>
-    * {
-        text-align: center;
-        color: #FFFFFF;
-        margin: 0;
-        padding: 0;
-    }
-    body {
-        background-color: #363636;
-        font-family: 'Roboto Mono', monospace;
-        font-size: 11pt;
-    }
-    a {
-        text-decoration: none;
-        color: white;
-        text-align: center;
-    }
-    a:hover {
-        color: GRAY;
-    }
-    a:active {
-        color: red;
-    }
-    .main {
-        width: 60%;
-        background-color: #262626;
-        float: none;
-        margin: 10px auto;
-        border: 10px solid #262626;
-        border-radius: 20px;
-    }
-    td {
-        font-size: 0.95rem;
-    }
-    table {
-        align-items: center;
-        float: none;
-        margin: 30px auto;
-        font-size: larger;
-    }
-    th {
-        padding: 5px 5px 10px;
-    }
-    td {
-        padding-left: 10px;
-        padding-right: 10px;
-    }
-    footer {
-        align-items: center;
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        padding-bottom: 20px;
-        padding-top: 20px;
-        min-height: 100px;
-        max-height: 250px;
-        background-color: #262626;
-    }
-</style>
 <body>
 <div class="main">
     <h1>Projects</h1><br>
