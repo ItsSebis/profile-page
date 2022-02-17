b423767648b1e0b57a7cc06d67bfa73c21af097e
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-02-17 17:26:25 +0100

    Seb32 Better #17 | generator-console.php 6. finish test

--- projects/Seb32/generator-console.php
+++ projects/Seb32/generator-console.php
@@ -19,45 +19,41 @@ function allowedSymbols() {
     return $syms;
 }
 
-function generate($amount=1) {
-    if ($amount <= 0) {
-        exit();
+$amount = 20;
+$lines = file("pattern.php");
+$str = "";
+$lineReading = 0;
+while (strpos($lines[$lineReading], ");") === false) {
+    $add = "";
+    if (strpos($lines[$lineReading+1], ");") !== false) {
+        $add = ",";
     }
-    $lines = file("pattern.php");
-    $str = "";
-    $lineReading = 0;
-    while (strpos($lines[$lineReading], ");") === false) {
-        $add = "";
-        if (strpos($lines[$lineReading+1], ");") !== false) {
-            $add = ",";
-        }
-        $str .= $lines[$lineReading].$add;
-        $lineReading++;
-    }
-
-    try {
-        for ($i=0;$i<$amount;$i++) {
-            $str.="array(";
-            foreach (allowedSymbols() as $key => $val) {
-                $str .= "\"$key\" => \"$val\"";
-                $array = allowedSymbols();
-                if ($val != end($array)) {
-                    $str .= ", ";
-                }
-            }
-            $str .= ")";
-            if ($i != $amount-1) {
-                $str.=",";
-            }
-        }
-
-        $str.="
-        );}";
-
-        $writer = fopen("pattern.php", "w+");
-        echo(fread($writer, 100));
-        fwrite($writer, $str);
-        fclose($writer);
-
-    } catch (Exception $e) {}
+    $str .= $lines[$lineReading].$add;
+    $lineReading++;
 }
+
+try {
+    for ($i=0;$i<$amount;$i++) {
+        $str.="array(";
+        foreach (allowedSymbols() as $key => $val) {
+            $str .= "\"$key\" => \"$val\"";
+            $array = allowedSymbols();
+            if ($val != end($array)) {
+                $str .= ", ";
+            }
+        }
+        $str .= ")";
+        if ($i != $amount-1) {
+            $str.=",";
+        }
+    }
+
+    $str.="
+    );}";
+
+    $writer = fopen("pattern.php", "w+");
+    echo(fread($writer, 100));
+    fwrite($writer, $str);
+    fclose($writer);
+
+} catch (Exception $e) {}
