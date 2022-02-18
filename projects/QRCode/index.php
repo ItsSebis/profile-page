2f0c0445eeeafdefbbecee6ff2fedf2daaa3f99c
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-02-18 19:54:28 +0100

    QRCode #5

--- projects/QRCode/index.php
+++ projects/QRCode/index.php
@@ -2,7 +2,7 @@
 require_once '../../phpqrcode/qrlib.php';
 
 $path = "imgs/";
-$file = $path.uniqid().".png";
+$file = $path."latest.png";
 
 $content = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 QRcode::png($content, $file);
