47c738b82ee29cfb9a9302aaa44c471019a7b052
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-02-24 16:39:02 +0100

    Werwolf Account link | dev #3

--- projects/Werwolf/clear.php
+++ projects/Werwolf/clear.php
@@ -6,7 +6,7 @@ foreach (allGames() as $game) {
     $mins = $diff->days * 24 * 60;
     $mins += ($diff->h+1) * 60;
     $mins += $diff->i;
-    echo($game["id"]." is $mins minutes old.");
+    echo($game["id"]." is $mins minutes old.\n");
     if ($mins >= 45) {
         foreach (gamePlayers($game["id"]) as $p) {
             delPlayer($p["name"], $game["id"]);
