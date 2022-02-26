5e83a7611d5085aed5e4b61fad86332c1261ca96
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-02-26 12:10:28 +0100

    Seb #34 | Generator Update

--- projects/Seb32/functions.php
+++ projects/Seb32/functions.php
@@ -5,6 +5,26 @@ require_once "./pattern.php";
 require_once "../publicFunc.php";
 #echo "Loaded pattern.php";
 
+/**
+ * @throws Exception
+ */
+function allowedSymbols() {
+    $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/()[]{}.,;+*~?=&\$<>";
+    $syms = array();
+    for ($i=0; $i<strlen($chars); $i++) {
+        $test = $chars[random_int(0, strlen($chars)-1)];
+        while (array_search($test, $syms) !== false) {
+            $test = $chars[random_int(0, strlen($chars)-1)];
+        }
+        $syms[$chars[$i]] = $test;
+    }
+
+    $syms[" "] = "%";
+    $syms[":"] = "#";
+
+    return $syms;
+}
+
 function getEncodedCount() {
     $count = getStats("seb32encoded")["value"];
     return reformatBIgInts($count);
@@ -113,4 +133,46 @@ function decode($str) {
     $str = seb32Decode($str);
     $str = base64_decode($str);
     return seb32Decode($str);
+}
+
+function generateNew($amount=1) {
+    $lines = file("pattern.php");
+    $str = "";
+    $lineReading = 0;
+    while (strpos($lines[$lineReading], ");") === false) {
+        $add = "";
+        if (strpos($lines[$lineReading+1], ");") !== false) {
+            $add = ",";
+        }
+        $str .= $lines[$lineReading].$add;
+        $lineReading++;
+    }
+
+    try {
+        for ($i=0;$i<$amount;$i++) {
+            $str.="
+        array(";
+            foreach (allowedSymbols() as $key => $val) {
+                $str .= "\"$key\" => \"$val\"";
+                $array = allowedSymbols();
+                if ($val != end($array)) {
+                    $str .= ", ";
+                }
+            }
+            $str .= ")";
+            if ($i != $amount-1) {
+                $str.=",";
+            }
+        }
+
+        $str.="
+    );
+    }";
+
+        $writer = fopen("pattern.php", "w+");
+        echo(fread($writer, 100));
+        fwrite($writer, $str);
+        fclose($writer);
+
+    } catch (Exception $e) {}
 }
--- projects/Seb32/generator.php
+++ projects/Seb32/generator.php
@@ -1,44 +1,7 @@
 <?php
+require_once "functions.php";
 
-/**
- * @throws Exception
- */
-function allowedSymbols() {
-    $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/()[]{}.,;+*~?=&\$<>";
-    $syms = array();
-    for ($i=0; $i<strlen($chars); $i++) {
-        $test = $chars[random_int(0, strlen($chars)-1)];
-        while (array_search($test, $syms) !== false) {
-            $test = $chars[random_int(0, strlen($chars)-1)];
-        }
-        $syms[$chars[$i]] = $test;
-    }
+generateNew();
 
-    $syms[" "] = "%";
-    $syms[":"] = "#";
-
-    return $syms;
-}
-
-$amount = 1;
-$str = "";
-
-try {
-    for ($i=0;$i<$amount;$i++) {
-        $str.="array(";
-        foreach (allowedSymbols() as $key => $val) {
-            $str .= "\"$key\" => \"$val\"";
-            $array = allowedSymbols();
-            if ($val != end($array)) {
-                $str .= ", ";
-            }
-        }
-        $str .= ")";
-        if ($i != $amount-1) {
-            $str.=",<br>";
-        }
-    }
-
-    echo $str;
-
-} catch (Exception $e) {}
+header("location: ./");
+exit();
