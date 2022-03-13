95debcf4a0bfc2de731316221eec15471dc28daf
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2022-03-13 21:06:33 +0100

    Projects now in Database

--- projects/index.php
+++ projects/index.php
@@ -6,6 +6,29 @@ $name = "Nicht angemeldet!";
 if (isset($_SESSION["id"]) && accountData($_SESSION["id"]) !== false) {
     $name = accountData($_SESSION["id"])["username"];
 }
+
+function allProjects() {
+    $con = con();
+    $sql = "SELECT * FROM projects ORDER BY `name` ASC;";
+    $stmt = mysqli_stmt_init($con);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ./?error=1&part=allProjects");
+        exit();
+    }
+
+    mysqli_stmt_execute($stmt);
+    $rs = mysqli_stmt_get_result($stmt);
+
+    $array = array();
+    if ($rs->num_rows > 0) {
+        while ($row = $rs->fetch_assoc()) {
+            $array[] = $row;
+        }
+    }
+    mysqli_stmt_close($stmt);
+    return $array;
+}
+
 ?>
     <!DOCTYPE html>
     <html lang="de">
@@ -18,7 +41,23 @@ if (isset($_SESSION["id"]) && accountData($_SESSION["id"]) !== false) {
     <link rel="icon" href="../img/title-bar.png">
     <link rel="stylesheet" href="style.css">
 </head>
+<style>
+    .project {
+        color: #00cccc;
+    }
+    .project:hover {
+        color: #007777;
+    }
+    .project:active {
+        color: #00ff9d;
+    }
+    td {
+        border: solid #030303;
+        border-radius: 4px;
+    }
+</style>
 <body>
+<a style="position: absolute; top: 10px; left: 10px;" href="..">← Back</a>
 <div class="main">
     <h1>Projekte</h1><br>
     <?php
@@ -43,8 +82,19 @@ if (isset($_SESSION["id"]) && accountData($_SESSION["id"]) !== false) {
         }
     }
 
-    echo '<a href="..">..</a><br><br>';
-    listFolderFiles('.');
+    #echo '<a href="..">..</a><br><br>';
+    #listFolderFiles('.');
+
+    echo "
+<table>
+<thead>
+<tr><th>Name</th><th>Erschaffer</th><th>Beschreibung</th></tr>
+</thead>
+";
+    foreach (allProjects() as $project) {
+        echo '<tr><td><a class="project" href="'.$project["dir"].'">'.$project["name"].'</a></td><td>'.$project["user"].'</td><td>'.$project["des"].'</td></tr>';
+    }
+    echo "</tbody></table>";
     ?>
 </div>
 <?php
