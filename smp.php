06fc1fa1ce52456ea4f6a6aed39e7887f52c0eab
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2023-01-21 22:07:20 +0100

    smp stats

--- smp.php
+++ smp.php
@@ -4,7 +4,7 @@ $all_players_json_stats_files = scandir("/opt/mc/java/server/stats/");
 $stats = array();
 foreach ($all_players_json_stats_files as $players_json_stats_file) {
     if (pathinfo($players_json_stats_file, PATHINFO_EXTENSION) == "json") {
-        $stats[pathinfo($players_json_stats_file, PATHINFO_FILENAME)] = file_get_contents("/opt/mc/java/server/".$players_json_stats_file);
+        $stats[pathinfo($players_json_stats_file, PATHINFO_FILENAME)] = file_get_contents("/opt/mc/java/server/stats/".$players_json_stats_file);
     } else {
         echo pathinfo($players_json_stats_file, PATHINFO_EXTENSION);
     }
