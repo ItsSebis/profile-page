bf65767ea65b145941c50c0b131b49c5ad1133ae
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2023-01-21 20:49:27 +0100

    smp stats

--- smp.php
+++ smp.php
@@ -1,9 +1,34 @@
 <?php
-require_once "header.php";
 
 $json_stats = file_get_contents("/opt/stats_TrackMe.json");
 
 $stats = json_decode($json_stats, true);
 
+if (!isset($_GET["api"])) {
+    require_once "header.php";
+} else {
+    echo $json_stats;
+    exit();
+}
+
 ?>
 
+<table>
+    <thead>
+    <tr>
+        <th>Player</th>
+    </tr>
+    </thead>
+    <?php
+    foreach ($stats as $player) {
+        echo "<tr>";
+        echo "<td>".$player['IGN']."</td>";
+        foreach ($player as $stat) {
+            if (is_array($stat)) {
+                // display stat
+            }
+        }
+        echo "</tr>";
+    }
+    ?>
+</table>
