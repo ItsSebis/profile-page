549771cca3248ffd35dbb6d8ec62bae687b114aa
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2023-01-21 22:26:48 +0100

    smp stats

--- smp.php
+++ smp.php
@@ -39,9 +39,7 @@ $all_statistics = json_decode($all_json_statistics, true);
     </thead>
     <?php
     foreach ($all_players_json_stats_files as $players_file) {
-        $player = json_decode(file_get_contents($players_file));
-        print_r($player);
-        exit();
+        $player = json_decode(file_get_contents("/opt/mc/java/server/stats/".$players_file));
         echo "<tr>";
         echo "<td>".$player['IGN']."</td>";
 
