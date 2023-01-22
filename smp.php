178d4854ae1873e3905069c005c750e3e801698b
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2023-01-22 20:46:47 +0100

    smp stats

--- smp.php
+++ smp.php
@@ -1,5 +1,60 @@
 <?php
 
+class Field_calculate {
+    const PATTERN = '/(?:\-?\d+(?:\.?\d+)?[\+\-\*\/])+\-?\d+(?:\.?\d+)?/';
+
+    const PARENTHESIS_DEPTH = 10;
+
+    public function calculate($input){
+        if(strpos($input, '+') != null || strpos($input, '-') != null || strpos($input, '/') != null || strpos($input, '*') != null){
+            //  Remove white spaces and invalid math chars
+            $input = str_replace(',', '.', $input);
+            $input = preg_replace('[^0-9\.\+\-\*\/\(\)]', '', $input);
+
+            //  Calculate each of the parenthesis from the top
+            $i = 0;
+            while(strpos($input, '(') || strpos($input, ')')){
+                $input = preg_replace_callback('/\(([^\(\)]+)\)/', 'self::callback', $input);
+
+                $i++;
+                if($i > self::PARENTHESIS_DEPTH){
+                    break;
+                }
+            }
+
+            //  Calculate the result
+            if(preg_match(self::PATTERN, $input, $match)){
+                return $this->compute($match[0]);
+            }
+            // To handle the special case of expressions surrounded by global parenthesis like "(1+1)"
+            if(is_numeric($input)){
+                return $input;
+            }
+
+            return 0;
+        }
+
+        return $input;
+    }
+
+    private function compute($input){
+        $compute = create_function('', 'return '.$input.';');
+
+        return 0 + $compute();
+    }
+
+    private function callback($input){
+        if(is_numeric($input[1])){
+            return $input[1];
+        }
+        elseif(preg_match(self::PATTERN, $input[1], $match)){
+            return $this->compute($match[0]);
+        }
+
+        return 0;
+    }
+}
+
 $all_players_json_stats_files = array_diff(scandir("/opt/mc/java/server/stats/"), array(".", ".."));
 $json_stats = "{";
 foreach ($all_players_json_stats_files as $players_json_stats_file) {
@@ -11,7 +66,7 @@ foreach ($all_players_json_stats_files as $players_json_stats_file) {
 }
 $json_stats .= "}";
 $stats = json_decode($json_stats, true);
-$display = array("Playtime" => array("PLAY_ONE_MINUTE", "/20/60/60", "h"));
+$display = array("Playtime" => array("name" => "PLAY_ONE_MINUTE", "factor" => "/20/60/60", "symbol" => "h"));
 
 if (!isset($_GET["api"])) {
     require_once "header.php";
@@ -67,8 +122,9 @@ $all_statistics = json_decode($all_json_statistics, true);
 
         foreach ($display as $namespace_key => $stat) {
             $data = $player[$namespace_key];
-            $value = round(eval('return '.$data['value'].$stat[1].';'), 2);
-            echo "<td class='".$namespace_key."' uuid='".$uuid."'>".$value.$stat[2]."</td>";
+            $Cal = new Field_calculate();
+            $value = round($Cal->calculate($data["value"].$stat["factor"]), 2);
+            echo "<td class='".$namespace_key."' uuid='".$uuid."'>".$value.$stat["symbol"]."</td>";
         }
         echo "</tr>";
     }
