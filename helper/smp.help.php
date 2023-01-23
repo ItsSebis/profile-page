4886fcf47ffe4d2af035bdea2cd1f0e6496c66f5
SebiGHG <59472813+SebiGHG@users.noreply.github.com>
2023-01-23 20:17:30 +0100

    smp stats

---
+++ helper/smp.help.php
@@ -0,0 +1,140 @@
+<?php
+
+class Field_calculate {
+    const PATTERN = '/(?:-?\d+(?:\.?\d+)?[+\-*\/])+-?\d+(?:\.?\d+)?/';
+
+    const PARENTHESIS_DEPTH = 10;
+
+    public function calculate($input){
+        if(strpos($input, '+') != null || strpos($input, '-') != null || strpos($input, '/') != null || strpos($input, '*') != null){
+            //  Remove white spaces and invalid math chars
+            $input = str_replace(',', '.', $input);
+            $input = preg_replace('[^0-9\.\+-\*/\(\)]', '', $input);
+
+            //  Calculate each of the parenthesis from the top
+            $i = 0;
+            while(strpos($input, '(') || strpos($input, ')')){
+                $input = preg_replace_callback('/\(([^()]+)\)/', 'self::callback', $input);
+
+                $i++;
+                if($i > self::PARENTHESIS_DEPTH){
+                    break;
+                }
+            }
+
+            //  Calculate the result
+            if(preg_match(self::PATTERN, $input, $match)){
+                return $this->compute($match[0]);
+            }
+            // To handle the special case of expressions surrounded by global parenthesis like "(1+1)"
+            if(is_numeric($input)){
+                return $input;
+            }
+
+            return 0;
+        }
+
+        return $input;
+    }
+
+    private function compute($input){
+        $compute = create_function('', 'return '.$input.';');
+
+        return 0 + $compute();
+    }
+
+    private function callback($input){
+        if(is_numeric($input[1])){
+            return $input[1];
+        }
+        elseif(preg_match(self::PATTERN, $input[1], $match)){
+            return $this->compute($match[0]);
+        }
+
+        return 0;
+    }
+}
+
+function getStatistics() {
+    return array(
+        "minecraft:animals_bred" =>                     array("name" => "Animals Bred", "factor" => "", "symbol" => ""),
+        "minecraft:armor_cleaned" =>                    array("name" => "Armor Pieces Cleaned", "factor" => "", "symbol" => ""),
+        "minecraft:banner_cleaned" =>                   array("name" => "Banners Cleaned", "factor" => "", "symbol" => ""),
+        "minecraft:open_barrel" =>                      array("name" => "Barrels Opened", "factor" => "", "symbol" => ""),
+        "minecraft:bell_ring" =>                        array("name" => "Bells Rung", "factor" => "", "symbol" => ""),
+        "minecraft:eat_cake_slice" =>                   array("name" => "Cake Slices Eaten", "factor" => "", "symbol" => ""),
+        "minecraft:fill_cauldron" =>                    array("name" => "Cauldrons Filled", "factor" => "", "symbol" => ""),
+        "minecraft:open_chest" =>                       array("name" => "Chests Opened", "factor" => "", "symbol" => ""),
+        "minecraft:damage_absorbed" =>                  array("name" => "Damage Absorbed", "factor" => "", "symbol" => ""),
+        "minecraft:damage_blocked_by_shield" =>         array("name" => "Damage Blocked By Shield", "factor" => "", "symbol" => ""),
+        "minecraft:damage_dealt" =>                     array("name" => "Damage Dealt", "factor" => "", "symbol" => ""),
+        "minecraft:damage_dealt_absorbed" =>            array("name" => "Damage Dealt (Absorbed)", "factor" => "", "symbol" => ""),
+        "minecraft:damage_dealt_resisted" =>            array("name" => "Damage Dealt (Resisted)", "factor" => "", "symbol" => ""),
+        "minecraft:damage_resisted" =>                  array("name" => "Damage Resisted", "factor" => "", "symbol" => ""),
+        "minecraft:damage_taken" =>                     array("name" => "Damage Taken", "factor" => "", "symbol" => ""),
+        "minecraft:inspect_dispenser" =>                array("name" => "Dispensers Searched", "factor" => "", "symbol" => ""),
+        "minecraft:climb_one_cm" =>                     array("name" => "Distance Climbed", "factor" => "/100", "symbol" => "m"),
+        "minecraft:crouch_one_cm" =>                    array("name" => "Distance Crouched", "factor" => "/100", "symbol" => "m"),
+        "minecraft:fall_one_cm" =>                      array("name" => "Distance Fallen", "factor" => "/100", "symbol" => "m"),
+        "minecraft:fly_one_cm" =>                       array("name" => "Distance Flown", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:sprint_one_cm" =>                    array("name" => "Distance Sprinted", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:swim_one_cm" =>                      array("name" => "Distance Swum", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:walk_one_cm" =>                      array("name" => "Distance Walked", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:walk_on_water_one_cm" =>             array("name" => "Distance Walked on Water", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:walk_under_water_one_cm" =>          array("name" => "Distance Walked under Water", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:boat_one_cm" =>                      array("name" => "Distance by Boat", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:aviate_one_cm" =>                    array("name" => "Distance by Elytra", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:horse_one_cm" =>                     array("name" => "Distance by Horse", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:minecart_one_cm" =>                  array("name" => "Distance by Minecart", "factor" => "/100000", "symbol" => "km"),
+        "minecraft:pig_one_cm" =>                       array("name" => "Distance by Pig", "factor" => "/100", "symbol" => "m"),
+        "minecraft:strider_one_cm" =>                   array("name" => "Distance by Strider", "factor" => "/100", "symbol" => "m"),
+        "minecraft:inspect_dropper" =>                  array("name" => "Droppers Searched", "factor" => "", "symbol" => ""),
+        "minecraft:open_enderchest" =>                  array("name" => "Ender Chests Opened", "factor" => "", "symbol" => ""),
+        "minecraft:fish_caught" =>                      array("name" => "Fish Caught", "factor" => "", "symbol" => ""),
+        "minecraft:leave_game" =>                       array("name" => "Games Quit", "factor" => "", "symbol" => ""),
+        "minecraft:inspect_hopper" =>                   array("name" => "Hoppers Searched", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_anvil" =>              array("name" => "Interactions with Anvil", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_beacon" =>             array("name" => "Interactions with Beacon", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_blast_furnace" =>      array("name" => "Interactions with Blast Furnace", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_brewingstand" =>       array("name" => "Interactions with Brewing Stand", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_campfire" =>           array("name" => "Interactions with Campfire", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_cartography_table" =>  array("name" => "Interactions with Cartography Table", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_crafting_table" =>     array("name" => "Interactions with Crafting Table", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_furnace" =>            array("name" => "Interactions with Furnace", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_grindstone" =>         array("name" => "Interactions with Grindstone", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_lectern" =>            array("name" => "Interactions with Lectern", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_loom" =>               array("name" => "Interactions with Loom", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_smithing_table" =>     array("name" => "Interactions with Smithing Table", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_smoker" =>             array("name" => "Interactions with Smoker", "factor" => "", "symbol" => ""),
+        "minecraft:interact_with_stonecutter" =>        array("name" => "Interactions with Stonecutter", "factor" => "", "symbol" => ""),
+        "minecraft:drop" =>                             array("name" => "Items Dropped", "factor" => "", "symbol" => ""),
+        "minecraft:enchant_item" =>                     array("name" => "Items Enchanted", "factor" => "", "symbol" => ""),
+        "minecraft:jump" =>                             array("name" => "Jumps", "factor" => "", "symbol" => ""),
+        "minecraft:mob_kills" =>                        array("name" => "Mob Kills", "factor" => "", "symbol" => ""),
+        "minecraft:play_record" =>                      array("name" => "Music Discs Played", "factor" => "", "symbol" => ""),
+        "minecraft:play_noteblock" =>                   array("name" => "Note Blocks Played", "factor" => "", "symbol" => ""),
+        "minecraft:tune_noteblock" =>                   array("name" => "Note Blocks Tuned", "factor" => "", "symbol" => ""),
+        "minecraft:deaths" =>                           array("name" => "Number of Deaths", "factor" => "", "symbol" => ""),
+        "minecraft:pot_flower" =>                       array("name" => "Plants Potted", "factor" => "", "symbol" => ""),
+        "minecraft:player_kills" =>                     array("name" => "Player Kills", "factor" => "", "symbol" => ""),
+        "minecraft:raid_trigger" =>                     array("name" => "Raids Triggered", "factor" => "", "symbol" => ""),
+        "minecraft:raid_win" =>                         array("name" => "Raids Won", "factor" => "", "symbol" => ""),
+        "minecraft:clean_shulker_box" =>                array("name" => "Shulker Boxes Cleaned", "factor" => "", "symbol" => ""),
+        "minecraft:open_shulker_box" =>                 array("name" => "Shulker Boxes Opened", "factor" => "", "symbol" => ""),
+        "minecraft:sneak_time" =>                       array("name" => "Sneak Time", "factor" => "", "symbol" => ""),
+        "minecraft:talked_to_villager" =>               array("name" => "Talked to Villagers", "factor" => "", "symbol" => ""),
+        "minecraft:target_hit" =>                       array("name" => "Targets Hit", "factor" => "", "symbol" => ""),
+        "minecraft:play_one_minute" =>                  array("name" => "Time Played", "factor" => "/20/3600", "symbol" => "h"),
+        "minecraft:time_since_death" =>                 array("name" => "Time Since Last Death", "factor" => "/20/3600", "symbol" => "h"),
+        "minecraft:time_since_rest" =>                  array("name" => "Time Since Last Rest", "factor" => "/20/3600", "symbol" => "h"),
+        "minecraft:total_world_time" =>                 array("name" => "Time with World Open", "factor" => "/20/3600", "symbol" => "h"),
+        "minecraft:sleep_in_bed" =>                     array("name" => "Times Slept in a Bed", "factor" => "", "symbol" => ""),
+        "minecraft:traded_with_villager" =>             array("name" => "Traded with Villagers", "factor" => "", "symbol" => ""),
+        "minecraft:trigger_trapped_chest" =>            array("name" => "Trapped Chests Triggered", "factor" => "", "symbol" => ""),
+        "minecraft:use_cauldron" =>                     array("name" => "Water Taken from Cauldron", "factor" => "", "symbol" => "")
+    );
+}
+
+if (isset($_GET["statistics"])) {
+    echo json_encode(getStatistics());
+}
--- smp.php
+++ smp.php
@@ -1,59 +1,39 @@
+<script type="text/javascript">
+    function httpGet(theUrl) {
+        let xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+        xmlHttp.send( null );
+        return xmlHttp.responseText;
+    }
+
+    function getPlayerTimeStr(uuid, data) {
+        let totalSecs = data[uuid]["minecraft:play_one_minute"]["value"]/20;
+        let days = Math.floor(totalSecs/86400);
+        let hours = Math.floor((totalSecs % 86400)/3600);
+        let minutes = Math.floor(((totalSecs % 86400)%3600)/60);
+        let seconds = Math.floor(totalSecs%60);
+        let timeStr = "";
+        if (days > 0) {
+            timeStr += days+"d "
+        }
+        if (hours > 0) {
+            timeStr += hours+"h "
+        }
+        if (minutes > 0) {
+            timeStr += minutes+"m "
+        }
+        if (seconds > 0) {
+            timeStr += seconds+"s"
+        }
+        return timeStr;
+    }
+
+    let data = JSON.parse(httpGet("https://sebis.net/smp.php?api"));
+    let all_statistics = JSON.parse(httpGet("https://sebis.net/smp.php?statistics"));
+</script>
 <?php
 
-class Field_calculate {
-    const PATTERN = '/(?:-?\d+(?:\.?\d+)?[+\-*\/])+-?\d+(?:\.?\d+)?/';
-
-    const PARENTHESIS_DEPTH = 10;
-
-    public function calculate($input){
-        if(strpos($input, '+') != null || strpos($input, '-') != null || strpos($input, '/') != null || strpos($input, '*') != null){
-            //  Remove white spaces and invalid math chars
-            $input = str_replace(',', '.', $input);
-            $input = preg_replace('[^0-9\.\+-\*/\(\)]', '', $input);
-
-            //  Calculate each of the parenthesis from the top
-            $i = 0;
-            while(strpos($input, '(') || strpos($input, ')')){
-                $input = preg_replace_callback('/\(([^()]+)\)/', 'self::callback', $input);
-
-                $i++;
-                if($i > self::PARENTHESIS_DEPTH){
-                    break;
-                }
-            }
-
-            //  Calculate the result
-            if(preg_match(self::PATTERN, $input, $match)){
-                return $this->compute($match[0]);
-            }
-            // To handle the special case of expressions surrounded by global parenthesis like "(1+1)"
-            if(is_numeric($input)){
-                return $input;
-            }
-
-            return 0;
-        }
-
-        return $input;
-    }
-
-    private function compute($input){
-        $compute = create_function('', 'return '.$input.';');
-
-        return 0 + $compute();
-    }
-
-    private function callback($input){
-        if(is_numeric($input[1])){
-            return $input[1];
-        }
-        elseif(preg_match(self::PATTERN, $input[1], $match)){
-            return $this->compute($match[0]);
-        }
-
-        return 0;
-    }
-}
+require_once "helper/smp.help.php";
 
 $all_players_json_stats_files = array_diff(scandir("/opt/mc/java/server/stats/"), array(".", ".."));
 $json_stats = "{";
@@ -66,7 +46,7 @@ foreach ($all_players_json_stats_files as $players_json_stats_file) {
 }
 $json_stats .= "}";
 $stats = json_decode($json_stats, true);
-$display = array("Games quit" => array("key" => "minecraft:leave_game", "name" => "LEAVE_GAME", "factor" => "", "symbol" => ""));
+$display = array("minecraft:leave_game");
 
 if (isset($_GET["api"])) {
     echo $json_stats;
@@ -74,12 +54,13 @@ if (isset($_GET["api"])) {
 } elseif (isset($_GET["display"])) {
     echo json_encode($display);
     exit();
+} elseif (isset($_GET["statistics"])) {
+    echo json_encode(getStatistics());
 } else {
     require_once "header.php";
 }
 
-$all_json_statistics = file_get_contents("/opt/mc/java/server/statistics.list");
-$all_statistics = json_decode($all_json_statistics, true);
+$all_statistics = getStatistics();
 
 ?>
 <h1 style="margin-top: 80px">SMP Stats</h1>
@@ -121,73 +102,41 @@ $all_statistics = json_decode($all_json_statistics, true);
         echo "<td class='time-td' uuid='".$uuid."'>".$timeStr."</td>";
 
         foreach ($display as $stat) {
-            $namespace_key = $stat["key"];
-            $data = $player[$namespace_key];
+            $data = $player[$stat];
             $Cal = new Field_calculate();
-            $value = round($Cal->calculate($data["value"].$stat["factor"]), 2);
-            echo "<td class='".$namespace_key."' uuid='".$uuid."'>".$value.$stat["symbol"]."</td>";
-            echo "<script>console.log('".$data["value"].$stat["factor"]." = ".$value."')</script>";
+            $value = round($Cal->calculate($data["value"].$all_statistics[$stat]["factor"]), 2);
+            echo "<td class='other-stat' namespace_key='".$stat."' uuid='".$uuid."'>".$value.$all_statistics[$stat]["symbol"]."</td>";
+            echo "<script>console.log('".$data["value"].$all_statistics[$stat]["factor"]." = ".$value."')</script>";
         }
         echo "</tr>";
     }
     ?>
 </table>
 <script>
-    function httpGet(theUrl) {
-        let xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-        xmlHttp.send( null );
-        return xmlHttp.responseText;
-    }
-
-    function myLoop() {         // create a loop function
-        setTimeout(function() {   // call a 3s setTimeout when the loop is called
+    function updateHomeLoop() { // create a loop function
+        setTimeout(function() { // call a 3s setTimeout when the loop is called
             // your code here
+            data = JSON.parse(httpGet("https://sebis.net/smp.php?api"));
 
             // update playtime
-            let json_data = httpGet("https://sebis.net/smp.php?api");
-            let data = JSON.parse(json_data);
             let players_playtimes = document.getElementsByClassName("time-td");
             for (let playtime of players_playtimes) {
                 let uuid = playtime.getAttribute("uuid");
-                let totalSecs = data[uuid]["minecraft:play_one_minute"]["value"]/20;
-                let days = Math.floor(totalSecs/86400);
-                let hours = Math.floor((totalSecs % 86400)/3600);
-                let minutes = Math.floor(((totalSecs % 86400)%3600)/60);
-                let seconds = Math.floor(totalSecs%60);
-                let timeStr = "";
-                if (days > 0) {
-                    timeStr += days+"d "
-                }
-                if (hours > 0) {
-                    timeStr += hours+"h "
-                }
-                if (minutes > 0) {
-                    timeStr += minutes+"m "
-                }
-                if (seconds > 0) {
-                    timeStr += seconds+"s"
-                }
-                playtime.innerText = timeStr;
+                playtime.innerText = getPlayerTimeStr(uuid, data);
             }
 
             // update other stats
-            let json_display = httpGet("https://sebis.net/smp.php?display");
-            let display = JSON.parse(json_display);
-            for (let stat of Object.values(display)) {
-                console.log(stat)
-                let tds = document.getElementsByClassName(stat["key"]);
-                for (let td of tds) {
-                    let uuid = td.getAttribute("uuid");
-                    let value = parseFloat(data[uuid][stat["key"]]["value"]+stat["factor"]);
-                    console.log(value);
-                    td.innerText = value+stat["symbol"];
-                }
+            let tds = document.getElementsByClassName("other-stat");
+            for (let td of tds) {
+                let key = td.getAttribute("namespace_key")
+                let uuid = td.getAttribute("uuid");
+                let value = parseFloat(data[uuid][key]["value"]+all_statistics[key]["factor"]);
+                td.innerText = value+all_statistics[key]["symbol"];
             }
 
-            myLoop();             // again which will trigger another
-        }, 100)
+            updateHomeLoop(); // again which will trigger another
+        }, 1000)
     }
 
-    myLoop();                   // start the loop
+    updateHomeLoop(); // start the loop
 </script>
